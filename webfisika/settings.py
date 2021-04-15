25fcae956466a12952b8e2518f401932ae76275e
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-04-15 23:22:13 +0700

    v1

--- webfisika/settings.py
+++ webfisika/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['penilaian-fisika.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['penilaian-fisika.herokuapp.com', 'localhost', '127.0.0.1', 'web-penilaian.herokuapp.com']
 
 
 # Application definition
