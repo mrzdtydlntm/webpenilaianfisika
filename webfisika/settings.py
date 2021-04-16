b6c6acdbc335c6f9e5759f41dc30975c352c3fcb
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-04-16 10:14:11 +0700

    v1

--- webfisika/settings.py
+++ webfisika/settings.py
@@ -140,6 +140,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# https://stackoverflow.com/questions/62302054/smtp-error-535-5-7-8-username-and-password-not-accepted-for-gmail-in-go
