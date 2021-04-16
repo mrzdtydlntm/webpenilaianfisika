f6b077bd0edf65f5b375aaeb331104cc43869b23
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-04-16 10:28:34 +0700

    v1

--- webfisika/settings.py
+++ webfisika/settings.py
@@ -139,8 +139,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
