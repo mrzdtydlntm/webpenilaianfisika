c74e298ef5f37a97a17722bc017bc8a6ff95dc70
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-05-03 20:01:09 +0700

    v1 launch

--- webfisika/settings.py
+++ webfisika/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from typing import cast
 from decouple import config
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
